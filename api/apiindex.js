5c6a860b85888c12a048c9c10307e27d13afbf12
indybelajar <indyagustin06@gmail.com>
2025-06-06 05:37:21 +0700

    revisi database

--- api/apiindex.js
+++
@@ -1,6 +0,0 @@
-// apiindex.js
-import app from './index.js'; // Mengimpor 'app' dari index.js
-import serverless from 'serverless-http';
-
-const handler = serverless(app);
-export default handler;
